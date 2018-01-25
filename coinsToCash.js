4ef368551003c7560ab4147800fa99ca61b8c26e
Mike Kluge <mkavarts@gmail.com>
2018-01-25 15:49:02 -0600

    final exercise

---
+++ coinsToCash.js
@@ -0,0 +1,16 @@
+var dollarAmount;
+
+const piggyBank = {
+    quarters: 400,
+    dimes: 1250,
+    nickels: 755,
+    pennies: 2333
+}
+
+
+dollarAmount = piggyBank.quarters/4 + piggyBank.dimes/10 + piggyBank.nickels/20 + piggyBank.pennies/1000;
+
+console.log(dollarAmount);
+
+
+ 
